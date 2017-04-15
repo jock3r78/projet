d8130a76c25363816ef1d04075a7838352d5a8af
jock3r78 <jock3r78@users.noreply.github.com>
2017-04-15 03:09:53 +0200

    update

---
+++ src/BlogBundle/Controller/IndexController.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jonathan
+ * Date: 14/04/2017
+ * Time: 08:46
+ */
+
+namespace BlogBundle\Controller;
+
+use BlogBundle\BlogBundle;
+use BlogBundle\Entity\Post;
+use BlogBundle\Form\CommentType;
+use BlogBundle\Form\PostType;
+use BlogBundle\Entity\User;
+use BlogBundle\Entity\Comment;
+use FOS\UserBundle\Doctrine\UserManager;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
+
+
+/**
+ * Post controller.
+ *
+ * @Route("/")
+ */
+class IndexController extends Controller
+{
+    /**
+     * alerte.
+     *
+     * @Route("/", name="index")
+     * @Method("GET")
+     */
+    public function IndexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $alerte = $em->getRepository('BlogBundle:Alerte')->getAllPosts();
+
+        return $this->render('BlogBundle:Default:index.html.twig', array(
+            'alerte' => $alerte,
+        ));
+    }
+
+}
---
+++ src/BlogBundle/Entity/Alerte.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace BlogBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Alerte
+ *
+ * @ORM\Table(name="alerte")
+ * @ORM\Entity(repositoryClass="BlogBundle\Repository\AlerteRepository")
+ */
+class Alerte
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="message", type="string", length=255)
+     */
+    private $message;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set message
+     *
+     * @param string $message
+     *
+     * @return Alerte
+     */
+    public function setMessage($message)
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    /**
+     * Get message
+     *
+     * @return string
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+}
+
